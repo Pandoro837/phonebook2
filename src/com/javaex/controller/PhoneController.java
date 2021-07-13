dc8d4df4ee1299ce1908ddece4fdd0d38d8fa399
young <mould837@naver.com>
2021-07-13 10:44:27 +0900

    switch문 들여쓰기

--- src/com/javaex/controller/PhoneController.java
+++ src/com/javaex/controller/PhoneController.java
@@ -25,90 +25,90 @@ public class PhoneController extends HttpServlet {
 //		System.out.println(action);
 		
 		switch(action) {
-		case "list":
-		{//변수명의 중복을 막기위해 지역변수로 만들 목적으로 case에 중괄호 사용
-			//리스트 가져오기
-			List<PersonVo> personList = phoneDao.getList();
+			case "list":
+			{//변수명의 중복을 막기위해 지역변수로 만들 목적으로 case에 중괄호 사용
+				//리스트 가져오기
+				List<PersonVo> personList = phoneDao.getList();
+				
+				//데이터 넣기 --> request 어트리뷰트에 데이터를 넣는다
+				request.setAttribute("pList", personList);
+				
+				//html로 표현 --> jsp로 포워드한다
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/list.jsp"); //해당 주소에서 일을 대신하게 한다(위치를 정확하게 입력해야한다)
+				rd.forward(request, response);	//리퀘스트와 리스폰스를 rd의 주소로 넘겨준다
+			}	
+				break;
 			
-			//데이터 넣기 --> request 어트리뷰트에 데이터를 넣는다
-			request.setAttribute("pList", personList);
+			case "insertForm":
+			{
+				//insertForm으로 포워드
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/insertForm.jsp");
+				rd.forward(request, response);
+			}	
+				break;
 			
-			//html로 표현 --> jsp로 포워드한다
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/list.jsp"); //해당 주소에서 일을 대신하게 한다(위치를 정확하게 입력해야한다)
-			rd.forward(request, response);	//리퀘스트와 리스폰스를 rd의 주소로 넘겨준다
-		}	
-			break;
-		
-		case "insertForm":
-		{
-			//insertForm으로 포워드
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/insertForm.jsp");
-			rd.forward(request, response);
-		}	
-			break;
-		
-		case "insert":
-		{
-			//파라미터를 꺼낸다
-			String name = request.getParameter("name");
-			String hp = request.getParameter("hp");
-			String company = request.getParameter("company");
-			
-			//vo로 묶어준다
-			PersonVo personInsert = new PersonVo(name, hp, company);
-//			System.out.println(personInsert);	Vo 확인용 출력
-			
-			//dao --> 저장
-			phoneDao.insert(personInsert);
-			
-			//리다이렉트
-			response.sendRedirect("/phonebook2/pbc?action=list");
-		}
-			break;
-			
-		case "delete":
-		{
-			//파라미터를 꺼낸다
-			int personId = Integer.parseInt(request.getParameter("personId"));
-			
-			//dao --> 삭제
-			phoneDao.delete(personId);
-			
-			//리다이렉트
-			response.sendRedirect("/phonebook2/pbc?action=list");
-		}	
-			break;
-			
-		case "updateForm":
-		{
-			//list의 수정 버튼으로 personId 값을 받아서 옮긴다
-			int personId = Integer.parseInt(request.getParameter("personId"));
-//			System.out.println(personId + "case:updateForm");	personId가 전송되지 않는 에러 발생, 확인용 출력 --> 해결
-			
-			//updateForm으로 포워드
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/updateForm.jsp");
-			request.setAttribute("personId", personId);	//forward를 통해 personId를 setAttribute로 보낸다
-			rd.forward(request, response);
-		}
-			break;
-			
-		case "update":
-		{
-			//파라미터를 꺼낸다
-			int personId = Integer.parseInt(request.getParameter("personId"));
-			String name = request.getParameter("name");
-			String hp = request.getParameter("hp");
-			String company = request.getParameter("company");
-			
-			//vo로 묶어준다
-			PersonVo personUpdate = new PersonVo(personId, name, hp, company);
-			
-			//dao --> 수정
-			phoneDao.update(personUpdate);
-			
-			//리다이렉트
-			response.sendRedirect("/phonebook2/pbc?action=list");
-		}	
+			case "insert":
+			{
+				//파라미터를 꺼낸다
+				String name = request.getParameter("name");
+				String hp = request.getParameter("hp");
+				String company = request.getParameter("company");
+				
+				//vo로 묶어준다
+				PersonVo personInsert = new PersonVo(name, hp, company);
+	//			System.out.println(personInsert);	Vo 확인용 출력
+				
+				//dao --> 저장
+				phoneDao.insert(personInsert);
+				
+				//리다이렉트
+				response.sendRedirect("/phonebook2/pbc?action=list");
+			}
+				break;
+				
+			case "delete":
+			{
+				//파라미터를 꺼낸다
+				int personId = Integer.parseInt(request.getParameter("personId"));
+				
+				//dao --> 삭제
+				phoneDao.delete(personId);
+				
+				//리다이렉트
+				response.sendRedirect("/phonebook2/pbc?action=list");
+			}	
+				break;
+				
+			case "updateForm":
+			{
+				//list의 수정 버튼으로 personId 값을 받아서 옮긴다
+				int personId = Integer.parseInt(request.getParameter("personId"));
+	//			System.out.println(personId + "case:updateForm");	personId가 전송되지 않는 에러 발생, 확인용 출력 --> 해결
+				
+				//updateForm으로 포워드
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/updateForm.jsp");
+				request.setAttribute("personId", personId);	//forward를 통해 personId를 setAttribute로 보낸다
+				rd.forward(request, response);
+			}
+				break;
+				
+			case "update":
+			{
+				//파라미터를 꺼낸다
+				int personId = Integer.parseInt(request.getParameter("personId"));
+				String name = request.getParameter("name");
+				String hp = request.getParameter("hp");
+				String company = request.getParameter("company");
+				
+				//vo로 묶어준다
+				PersonVo personUpdate = new PersonVo(personId, name, hp, company);
+				
+				//dao --> 수정
+				phoneDao.update(personUpdate);
+				
+				//리다이렉트
+				response.sendRedirect("/phonebook2/pbc?action=list");
+			}	
 			break;
 		}
 		
